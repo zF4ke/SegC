41c1b081351e2198668a7242b9650315a2ab8f60
fc59886 <fc59886@alunos.ciencias.ulisboa.pt>
2025-03-28 17:41:36 +0000

    build.sh compiling into /bin

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "Main-Class: client.MySharingClient" > build/manifest/client_manifest.mf
 javac -d build src/client/*.java src/server/*.java src/server/*/*.java
 
 # Criar os arquivos JAR
-jar --create --file build/libs/mySharingServer.jar --manifest build/manifest/server_manifest.mf -C build .
-jar --create --file build/libs/mySharingClient.jar --manifest build/manifest/client_manifest.mf -C build .
+jar --create --file build/bin/mySharingServer.jar --manifest build/manifest/server_manifest.mf -C build .
+jar --create --file build/bin/mySharingClient.jar --manifest build/manifest/client_manifest.mf -C build .
 
-echo "Build concluído! JARs estão em build/libs"
+echo "Build concluído! JARs estão em build/bin"
