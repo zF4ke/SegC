8761a482ce72243ed715ac0c07cbf3aba6bcc6bd
zF4ke <pedrohsilva955@gmail.com>
2025-03-28 17:26:49 +0000

    compilar

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Criar diretórios de compilação
+rm -rf build/
+mkdir -p build/client build/server build/bin build/manifest
+
+# Manisfest
+echo "Main-Class: server.MySharingServer" > build/manifest/server_manifest.mf
+echo "Main-Class: client.MySharingClient" > build/manifest/client_manifest.mf
+
+# Compilar os arquivos Java
+javac -d build src/client/*.java src/server/*.java src/server/*/*.java
+
+# Criar os arquivos JAR
+jar --create --file build/libs/mySharingServer.jar --manifest build/manifest/server_manifest.mf -C build .
+jar --create --file build/libs/mySharingClient.jar --manifest build/manifest/client_manifest.mf -C build .
+
+echo "Build concluído! JARs estão em build/libs"
